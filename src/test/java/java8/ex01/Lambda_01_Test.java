720cf6d3acb35a2ebc27a3c1bcf2c5d5d3e48ca5
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-02 16:55:03 +0200

    ex02 lambdas

--- src/test/java/java8/ex01/Lambda_01_Test.java
+++ src/test/java/java8/ex01/Lambda_01_Test.java
@@ -4,76 +4,103 @@ import java8.data.Data;
 import java8.data.Person;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
-
 
 /**
  * Exercice 01 - Filter
  */
 public class Lambda_01_Test {
 
-    // tag::PersonPredicate[]
-    interface PersonPredicate {
-        boolean test(Person p);
-    }
-    // end::PersonPredicate[]
+	// tag::PersonPredicate[]
+	interface PersonPredicate {
+		boolean test(Person p);
+	}
+	// end::PersonPredicate[]
 
-    // tag::filter[]
-    private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
-        // TODO implementer la méthode
-        return null;
-    }
-    // end::filter[]
+	// tag::filter[]
+	private List<Person> filter(List<Person> persons, PersonPredicate predicate) {
+		// TODO implementer la méthode
+		List<Person> result = new ArrayList<>();
 
+		for (Person p : persons) {
 
-    // tag::test_filter_by_age[]
-    @Test
-    public void test_filter_by_age() throws Exception {
+			if (predicate.test(p)) {
+				result.add(p);
+			}
 
-        List<Person> personList = Data.buildPersonList(100);
+		}
+		return result;
+	}
+	// end::filter[]
 
-        // TODO result ne doit contenir que des personnes adultes (age >= 18)
-        List<Person> result = filter(personList, null);
+	// tag::test_filter_by_age[]
+	@Test
+	public void test_filter_by_age() throws Exception {
 
-        assert result.size() == 83;
+		List<Person> personList = Data.buildPersonList(100);
 
-        for (Person person : result) {
-            assert person.getAge() > 17;
-        }
-    }
-    // end::test_filter_by_age[]
+		// TODO result ne doit contenir que des personnes adultes (age >= 18)
+		List<Person> result = filter(personList, new PersonPredicate() {
 
-    // tag::test_filter_by_firstname[]
-    @Test
-    public void test_filter_by_firstname() throws Exception {
+			@Override
+			public boolean test(Person p) {
 
-        List<Person> personList = Data.buildPersonList(100);
+				return p.getAge() >= 18;
+			}
+		});
 
-        // TODO result ne doit contenir que des personnes dont le prénom est "first_10"
-        List<Person> result = filter(personList, null);
+		assert result.size() == 83;
 
-        assert result.size() == 1;
-        assert result.get(0).getFirstname().equals("first_10");
+		for (Person person : result) {
+			assert person.getAge() > 17;
+		}
+	}
+	// end::test_filter_by_age[]
 
-    }
-    // end::test_filter_by_firstname[]
+	// tag::test_filter_by_firstname[]
+	@Test
+	public void test_filter_by_firstname() throws Exception {
 
-    // tag::test_filter_by_password[]
-    @Test
-    public void test_filter_by_password() throws Exception {
+		List<Person> personList = Data.buildPersonList(100);
 
-        List<Person> personList = Data.buildPersonList(100);
+		// TODO result ne doit contenir que des personnes dont le prénom est
+		// "first_10"
+		List<Person> result = filter(personList, new PersonPredicate(){
 
-        String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
+			@Override
+			public boolean test(Person p) {
+				
+				return p.getFirstname() == "first_10";
+			}
+			
+		});
 
-        // TODO result ne doit contenir que les personnes dont l'age est > 49 et dont le hash du mot de passe correspond à la valeur de la variable passwordSha512Hex
-        // TODO Pour obtenir le hash d'un mot, utiliser la méthode DigestUtils.sha512Hex(mot)
-        List<Person> result = filter(personList, null);
+		assert result.size() == 1;
+		assert result.get(0).getFirstname().equals("first_10");
 
-        assert result.size() == 6;
-        for (Person person : result) {
-            assert person.getPassword().equals("test");
-        }
-    }
-    // end::test_filter_by_password[]
+	}
+	// end::test_filter_by_firstname[]
+
+	// tag::test_filter_by_password[]
+	@Test
+	public void test_filter_by_password() throws Exception {
+
+		List<Person> personList = Data.buildPersonList(100);
+
+		String passwordSha512Hex = "ee26b0dd4af7e749aa1a8ee3c10ae9923f618980772e473f8819a5d4940e0db27ac185f8a0e1d5f84f88bc887fd67b143732c304cc5fa9ad8e6f57f50028a8ff";
+
+		// TODO result ne doit contenir que les personnes dont l'age est > 49 et
+		// dont le hash du mot de passe correspond à la valeur de la variable
+		// passwordSha512Hex
+		// TODO Pour obtenir le hash d'un mot, utiliser la méthode
+		// DigestUtils.sha512Hex(mot)
+		List<Person> result = filter(personList, null);
+
+		assert result.size() == 6;
+		for (Person person : result) {
+			assert person.getPassword().equals("test");
+		}
+	}
+	// end::test_filter_by_password[]
 }
