2fab5f3d75bcbbf9efe0db44df1701114e5d67f2
Olivier LE HENAFF <lehenaff.olivier@hotmail.fr>
2017-10-03 11:56:28 +0200

    transformation liste personne -> compte

--- src/test/java/java8/ex02/Lambda_02_Test.java
+++ src/test/java/java8/ex02/Lambda_02_Test.java
@@ -27,13 +27,13 @@ public class Lambda_02_Test {
     	// etape 1 => créer une liste de comptes vide
     	List<Account> comptes = new ArrayList<>();
     	// etape 2 => itérer sur la liste de personnes
+    	// pour chaque personne	
+		// transformer l'objet personne en Account via mapper.map(objPersonne)
+		// ajouter l'objet Account à la liste des comptes
     		for (Person p : personList){
     			comptes.add(mapper.map(p));
     		}
-    		// pour chaque personne
     		
-    		// transformer l'objet personne en Account via mapper.map(objPersonne)
-    		// ajouter l'objet Account à la liste des comptes
     	
     	// etape 3 => retourner la liste de comptes
     	
@@ -50,11 +50,19 @@ public class Lambda_02_Test {
         List<Person> personList = Data.buildPersonList(100);
 
         // TODO transformer la liste de personnes en liste de comptes
+        
         // TODO tous les objets comptes ont un solde à 100 par défaut
-        List<Account> result = map(personList, null);
-
-        assert result.size() == personList.size();
-        for (Account account : result) {
+        List<Account> comptes = map(personList, p -> {
+        	Account a = new Account();
+        	a.setOwner(p);
+        	a.setBalance(100);
+        	return a;
+        	
+        });
+        
+        
+        assert comptes.size() == personList.size();
+        for (Account account : comptes) {
             assert account.getBalance().equals(100);
             assert account.getOwner() != null;
         }
