d15c27668ea33d131df774445f1673fbe60f2075
Olivier Le Hénaff <lehenaff.olivier@hotmail.fr>
2017-10-05 21:23:16 +0200

    fonctions ex03 checked

--- src/test/java/java8/ex03/Function_03_Test.java
+++ src/test/java/java8/ex03/Function_03_Test.java
@@ -16,7 +16,15 @@ public class Function_03_Test {
     // TODO l'enfant possède le prenom "<PRENOM_PERE> <PRENOM_MERE>"
     // TODO l'age de l'enfant est 0
     // TODO le mot de passe de l'enfant est null
-    BinaryOperator<Person> makeAChild = null;
+    BinaryOperator<Person> makeAChild = (father, mother)-> {
+    	
+    	Person child = new Person();
+    	child.setFirstname(father.getFirstname() + mother.getFirstname());
+    	child.setLastname(father.getLastname());
+    	child.setAge(0);
+    	child.setPassword(null);    	
+    	return child;
+    };
     //  end::makeAChild[]
 
 
@@ -27,7 +35,8 @@ public class Function_03_Test {
         Person mother = new Person("Aline", "Lebreton", 22, "alino");
 
         // TODO compléter le test pour qu'il soit passant
-        Person child = null;
+        Person child = new Person("John Aline", "France", 0, null);
+        
 
         assert child.getFirstname().equals("John Aline");
         assert child.getLastname().equals("France");
